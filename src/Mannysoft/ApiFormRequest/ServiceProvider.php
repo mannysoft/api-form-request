01c8152c348a5347d176d3ccaf5d917d1f4c151a
Manny Isles <manny.isles@gmail.com>
2018-05-04 12:10:10 +0800

    Update ServiceProvider.php

--- src/Mannysoft/ApiFormRequest/ServiceProvider.php
+++ src/Mannysoft/ApiFormRequest/ServiceProvider.php
@@ -4,6 +4,7 @@ namespace Mannysoft\ApiFormRequest;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Mannysoft\ApiFormRequest\ApiFormRequest;
+use Mannysoft\ApiFormRequest\Console\Command\RequestMakeCommand;
 
 class ServiceProvider extends BaseServiceProvider {
     
@@ -14,6 +15,11 @@ class ServiceProvider extends BaseServiceProvider {
      */
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                RequestMakeCommand::class,
+            ]);
+        }
         // Credits
         // https://medium.com/@Sirolad/laravel-5-5-api-form-request-validation-errors-d49a85cd29f2
         // http://cwhite.me/laravel-api-form-request-validation-errors/
